97ff7d5a5608486980b3d4eaa5130828425f1210
Take Vos <info@takevos.nl>
2024-02-29 16:11:35 +0100

    add copyright, and disable fopen security warnings

--- src/hikotest/hikotest.cpp
+++ src/hikotest/hikotest.cpp
@@ -1,5 +1,8 @@
+// Copyright Take Vos 2024.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 
-
+#define _CRT_SECURE_NO_WARNINGS
 #include "hikotest.hpp"
 #include <utility>
 #include <print>
--- src/hikotest/hikotest.hpp
+++ src/hikotest/hikotest.hpp
@@ -1,4 +1,9 @@
+// Copyright Take Vos 2024.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 
+#ifndef HIKOTEST_HPP
+#define HIKOTEST_HPP
 
 #include <concepts>
 #include <utility>
@@ -906,3 +911,6 @@ struct suite {
 };
 
 } // namespace test
+
+#endif
+
